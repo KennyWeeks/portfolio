d8a0b4e1504df57bd72e29b4bb31d821db943a73
KennyWeeks <weeksgit@gmail.com>
2023-07-02 17:29:56 -0700

    Made some changes to the styles, just want to see it on my phone.

---
+++ portfolio/src/lib/menu.svelte
@@ -0,0 +1,35 @@
+<script lang="ts"></script>
+
+<div id="menu">
+
+    <div class="menu_bar"></div>
+    <div class="menu_bar"></div>
+
+</div>
+
+<style lang="scss">
+        @import "../styles/theme.scss";
+
+    #menu {
+        position:absolute;
+        top:15px;
+        right:15px;
+        z-index:10000;
+        width:35px;
+        height:18px;
+        @include flexCol;
+
+        .menu_bar {
+            width:100%;
+            height:7px;
+            background-color:$offBlack;
+            border-radius:10px;
+
+        }
+
+        :nth-child(2) {
+            width:50%;
+            margin-left:50%;
+        }
+    }
+</style>
--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+    import "../styles/fonts.css"
+    import Menu from "$lib/menu.svelte";
+
+</script>
+
+<Menu/>
+
+<div id="landing">
+
+    <div id="intro">
+
+        <h3 id="intro_text">Hi,<br> my name is Kenny Weeks. <br> Welcome to my ... </h3>
+
+        <img src="images/test.svg" alt="portfolio_text"/>
+
+    </div>
+
+</div>
+
+<style lang="scss">
+    @import "../styles/theme.scss";
+
+    #landing {
+        width:100vw;
+        height:100vh;
+        background-color:$offWhite;
+        overflow:hidden;
+        @include flexCenter;
+
+        #intro {
+            width:100vw;
+            overflow:hidden;
+
+            h3 {
+                font-family:"Goldoni";
+                font-size:25px;
+                margin-left:10px;
+            }
+        }
+    }
+
+</style>
