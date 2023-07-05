c38a836867c42219047d9b544352890e2c9fc07a
KennyWeeks <weeksgit@gmail.com>
2023-07-05 13:20:36 -0700

    Starting project animation
    Will start adding project animation when scrolled to here.

--- src/lib/sections/projects.svelte
+++ src/lib/sections/projects.svelte
@@ -15,12 +15,21 @@
         padding:10px 0px;
 
         #projects_floating_tab {
-            width:90vw;
+
+            @media only screen and (min-width:811px) {
+                width:100vw;
+                margin-left:0vw;
+            }
+
+            @media only screen and (max-width:810px) {
+                width:90vw;
+                margin-left:5vw;
+            }
             height:300vh;/*This will be a dummy height, will size to content*/
             background-color:$offBlack;
             border-radius:30px;
             margin-top:calc(18vw);
-            margin-left:5vw;
+            
         }
     }
 </style>
