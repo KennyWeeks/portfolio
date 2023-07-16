0962ee2017f3e2844a65b23a169916fe49e22f7d
KennyWeeks <weeksgit@gmail.com>
2023-07-16 16:14:40 -0700

    Some changes to the about page, will add more later.

--- src/lib/sections/about.svelte
+++ src/lib/sections/about.svelte
@@ -7,7 +7,14 @@
 
         let about_summary : any = document.getElementById("about-summary");
         let education : any = document.getElementById("education");
-        education.style.height = about_summary.clientHeight + "px";
+        let aboutContentArea : any = document.getElementById("about-content-area");
+        let about : any = document.getElementById("about");
+        /*window.addEventListener("resize", ()=>{
+            if(window.innerWidth <= 1250) {
+
+                aboutContentArea.style.transform = "scale(" + (0.9 * window.innerWidth / 1200) + ")";
+            }
+        });*/
     });
 </script>
 
@@ -15,40 +22,46 @@
 
     <div id="about-content-area">
 
-        <img src="{path}/images/about_ring.png" alt="about ring"/>
-
-        <div id="about-summary">
-
-            <div id="picture"></div>
-
-            <p>I am a computer science
-                graduate, currently attending
-                the University of California,
-                Irvine. I have a Bachelors of Science
-                in Computer Science from UNLV and two
-                minors in mathematics and physics. I’ve
-                been programming since high school, and I
-                enjoy working at the lowest level of a 
-                computer, specifically O.S. and compilers.</p>
-
+        <div id="about_image">
+            <img src="{path}/images/about_ring.png" alt="about ring"/>
         </div>
 
-        <div id="education">
+        <div id="about-summary-and-edu">
 
-            <div class="individual-edu">
-                <p>
-                    <span class="university">University of Nevada, Las Vegas</span><br>
-                    <span class="gpa">GPA: 3.657</span><br>
-                    <span class="major">Bachelors in Computer Science</span><br>
-                    <span class="minor">Minors in Physics and Mathematics</span><br>
-                </p>
+            <div id="about-summary">
+
+                <div id="picture"></div>
+
+                <p>I am a computer science
+                    graduate, currently attending
+                    the University of California,
+                    Irvine. I have a Bachelors of Science
+                    in Computer Science from UNLV and two
+                    minors in mathematics and physics. I’ve
+                    been programming since high school, and I
+                    enjoy working at the lowest level of a 
+                    computer, specifically O.S. and compilers.</p>
+
             </div>
-            <div class="individual-edu">
-                <p>
-                    <span class="university">University of California, Irvine</span><br>
-                    <span class="gpa">GPA: 3.9</span><br>
-                    <span class="major">Masters in Computer Science</span>
-                </p>
+
+            <div id="education">
+
+                <div class="individual-edu">
+                    <p>
+                        <span class="university">University of Nevada, Las Vegas</span><br>
+                        <span class="gpa">GPA: 3.657</span><br>
+                        <span class="major">Bachelors in Computer Science</span><br>
+                        <span class="minor">Minors in Physics and Mathematics</span><br>
+                    </p>
+                </div>
+                <div class="individual-edu">
+                    <p>
+                        <span class="university">University of California, Irvine</span><br>
+                        <span class="gpa">GPA: 3.9</span><br>
+                        <span class="major">Masters in Computer Science</span>
+                    </p>
+                </div>
+
             </div>
 
         </div>
@@ -119,103 +132,162 @@
         padding:250px 0px 200px 0px;
         overflow:hidden;
         cursor:default;
-        @include flexCenter;
+        @include randomTest((">800px")) {
+            @include flexCenter;
+        }
 
         #about-content-area {
-            width:1216px;
-            display:flex;
-            justify-content:space-between;
+            @include flexRow;
             position:relative;
             flex-flow:row wrap;
 
-            img {
+            @include randomTest((">800px")) {
+                width:1216px;
+            }
+
+            @include randomTest(("<800px")) {
+                width:90vw;
+                margin-left:5vw;
+                box-shadow:inset 0 0 0 2px #00ff00;
+            }
+
+            #about_image {
                 position:absolute;
-                width:500px;
-                height:500px;
-                top:-250px;
-                left:calc((800px - 500px) / 2);
-                animation: rotate 10s linear infinite;
-            }
-
-            #about-summary {
-                position:relative;
-                border-radius:$text-block-border-radius;
-                @include randomTest((">1200px")) {
-                    //So this will be the desktop styles
-                    width:800px;
-                    margin:auto;
-                    background-color:$offBlack;
-                    padding-top:170px;
-                    padding-bottom:20px;
+                width:340px;
+                height:340px;
+                top:-170px;
+                @include randomTest((">800px")) {
+                    left:calc((800px - 340px) / 2);
                 }
-
-                #picture {
-                    width:320px;
-                    height:320px;
-                    position:absolute;
-                    border-radius:50%;
-                    background-color:$offWhite;
-                    top:-170px;
-                    left:50%;
-                    transform:translate(-50%);
-                    border:20px solid $offBlack;
+                @include randomTest(("<800px")) {
+                    left:calc((90vw - 340px) / 2);
                 }
+                border-radius:50%;
+                box-shadow:inset 0 0 0 2px #000;
 
-                p {
-                    color:$offWhite;
-                    font-family:$font-family;
-                    width:90%;
-                    margin-left:5%;
-                    @include randomTest((">1200px")) {
-                        font-size:25px;
-                    }
+                img {
+                    width:500px;
+                    animation: rotate 10s linear infinite;
+                    margin-left:calc((340px - 500px) / 2);
+                    margin-top:calc((350px - 500px) / 2);
                 }
             }
 
-            #education {
-                margin:auto;
-                height:100%;
+            #about-summary-and-edu {
+                width:100%;
+                @include flexRow;
+                flex-flow:row wrap;
 
-                @include randomTest((">1200px")) {
-                    width:400px;
-                    display:block;
-                }
-
-                .individual-edu {
+                #about-summary {
+                    position:relative;
                     background-color:$offBlack;
                     border-radius:$text-block-border-radius;
-                    color:$offWhite;
-                    font-family:$font-family;
-                    font-size:25px;
-                    @include flexCenter;
+                    padding-top:170px;
+                    padding-bottom:20px;
+                    @include randomTest((">800px")) {
+                        //So this will be the desktop styles
+                        width:800px;
+                        margin:auto;
+                        
+                        ;
+                        margin-left:0px;
+                    }
 
-                    @include randomTest((">1200px")) {
-                        width:400px;
-                        height:49%;
+                    @include randomTest(("<800px")) {
+                        width:100%;
+                    }
 
-                        &:nth-of-type(2) {
-                            margin-top:2%;
+                    #picture {
+                        width:320px;
+                        height:320px;
+                        position:absolute;
+                        border-radius:50%;
+                        background-color:$offWhite;
+                        top:-170px;
+                        left:50%;
+                        transform:translate(-50%);
+                        border:20px solid $offBlack;
+                    }
+
+                    p {
+                        color:$offWhite;
+                        font-family:$font-family;
+                        width:90%;
+                        margin-left:5%;
+                        @include randomTest((">800px")) {
+                            font-size:25px;
+                        }
+
+                        @include randomTest(("<800px")) {
+                            font-size:20px;
                         }
                     }
+                }
 
-                    .university {
-                        font-weight:bolder;
+                #education {
+                    margin:auto;
+                    height:100%;
+                    margin-right:0px;
+
+
+                    @include randomTest((">800px")) {
+                        width:407px;
+                        display:block;
                     }
 
-                    .gpa {
-                        font-size:18px;
+                    @include randomTest(("<800px")) {
+                        width:100%;
+                        height:auto;
+                        margin-top:7px;
+                        @include flexRow;
                     }
 
-                    .major {
-                        font-size:20px;
-                        font-style:italic;
-                    }
+                    .individual-edu {
+                        background-color:$offBlack;
+                        border-radius:$text-block-border-radius;
+                        color:$offWhite;
+                        font-family:$font-family;
+                        
+                        @include flexCenter;
+                        overflow:hidden;
+                        margin-top:0px;
 
-                    .minor {
-                        font-size:18px;
-                        font-style:italic;
-                    }
+                        @include randomTest((">800px")) {
+                            width:400px;
+                            height:49%;
+                            font-size:25px;
 
+                            &:nth-of-type(2) {
+                                margin-top:2%;
+                            }
+                        }
+
+                        @include randomTest(("<800px")) {
+                            width:49.5%;
+                            padding:20px 0px;
+                            font-size:20px;
+
+                        }
+
+                        .university {
+                            font-weight:bolder;
+                        }
+
+                        .gpa {
+                            font-size:18px;
+                        }
+
+                        .major {
+                            font-size:20px;
+                            font-style:italic;
+                        }
+
+                        .minor {
+                            font-size:18px;
+                            font-style:italic;
+                        }
+
+                    }
                 }
             }
 
@@ -227,6 +299,7 @@
                 margin-top:8px;
                 font-family:$font-family;
                 color:$offWhite;
+                position:relative;
                 @include flexRow;
 
                 .vert-divide {
@@ -243,7 +316,6 @@
                     width:33.33%;
                     height:400px;
                     position:relative;
-                    @include flexCenter;
 
                     .individual-skill-contain {
 
@@ -261,11 +333,13 @@
                             }
                         }
                         width:80%;
+                        margin:auto;
+                        margin-top:10%;
                         .individual-skill {
                             display:inline-block;
                             border-radius:50px;
                             box-shadow:inset 0 0 0 2px $offWhite;
-                            @include randomTest((">1200px")) {
+                            @include randomTest((">800px")) {
                                 font-size:25px;
                                 padding:10px 20px;
                                 margin-right:15px;
